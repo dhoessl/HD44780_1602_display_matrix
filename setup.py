ad9989dfbb1aa940e466446d98e70736ff95742e
Dominic Hößl <dominic.hoessl@noris.de>
2025-03-20 07:27:54 +0100

    fix build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='HD44780_1602_display_matrix',
+    name='1602_display_matrix',
     version='0.1.0',
     description='library to manage multiple 1602 LCD displays in a matrix',
     url='https://github.com/dhoessl/HD44780_1602_display_matrix',
