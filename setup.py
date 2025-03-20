f47c76f2a412139d2a63c99db8b0176146595ccf
Dominic Hößl <dominic.hoessl@noris.de>
2025-03-20 07:40:27 +0100

    push fix version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lcd_i2c_display_matrix',
-    version='0.1.0',
+    version='0.1.1',
     description='library to manage multiple 1602 LCD displays in a matrix',
     url='https://github.com/dhoessl/HD44780_1602_display_matrix',
     author="Dominic Hößl",
