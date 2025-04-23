e31ac05d7095c214cd9a0cc7c22762d08b4eefbc
Dominic Hößl <dominichoessl@gmail.com>
2025-04-23 02:14:37 +0200

    LCDMatrix got renamed to Matrix

--- lcd_i2c_display_matrix/__init__.py
+++ lcd_i2c_display_matrix/__init__.py
@@ -1,3 +1,3 @@
-from .matrix import LCDMatrix
+from .matrix import Matrix
 from .lcd_websocket_listener import MatrixCommandReceiver
 from .lcd_websocket_sender import MatrixCommandSender
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lcd_i2c_display_matrix',
-    version='0.3.1',
+    version='0.3.1-0',
     description='library to manage multiple 1602 LCD displays in a matrix',
     url='https://github.com/dhoessl/HD44780_1602_display_matrix',
     author="Dominic Hößl",
